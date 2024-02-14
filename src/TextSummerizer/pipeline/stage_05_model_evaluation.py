3790e3aba89aaa5b759010cc4d28f801c86b28ce
Devesh Sharma <kaluklt1496@gmail.com>
2024-02-14 14:31:11 +0530

    data transformation, model trainer, model evalauation, prediction, app

---
+++ src/TextSummerizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,15 @@
+from TextSummerizer.config.configuration import ConfigurationManager
+from TextSummerizer.components.model_evaluation import ModelEvaluation
+
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
